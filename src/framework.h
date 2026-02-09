2ae974eb14bd1bbc273503791acf1774ccaca941
Alexander Frick <Alex313031@gmail.com>
2026-02-09 01:42:39 -0600

    better windows SDK version defines defaults

--- src/framework.h
+++ src/framework.h
@@ -11,9 +11,7 @@
  #endif
 #endif
 
-#include "version.h"
-
-//#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+#include "./version.h"
 
 // We need to define _UNICODE and UNICODE for TCHAR
 #ifndef UNICODE
@@ -24,9 +22,40 @@
  #define _UNICODE
 #endif
 
+#if  defined(__clang__) && defined(_UNICODE)
+ #pragma code_page(65001) // UTF-8
+#endif // __clang__
+
 #ifndef __FUNC__
  #define __FUNC__ __func__
 #endif
+
+#ifndef __MINGW32__
+ #include <WinSDKVer.h> // Doesn't exist in MinGW
+#endif // __MINGW32__
+
+#ifndef _WIN32_WINNT
+ #define _WIN32_WINNT 0x0500 // Windows 2000
+#endif // _WIN32_WINNT
+#ifndef WINVER
+ #define WINVER 0x0500 // Same as above
+#endif // WINVER
+#ifndef _WIN64_WINNT
+ #define _WIN64_WINNT 0x0502 // Minimum version for 64 bit, Windows Server 2003
+#endif // _WIN64_WINNT
+#ifndef _WIN32_IE
+ #define _WIN32_IE 0x0501 // Minimum Internet Explorer version for common controls
+#endif // _WIN32_IE
+
+#if _WIN32_WINNT <= 0x0600 // If we are less than Windows 7, use old ATL for safety
+ #ifndef _ATL_XP_TARGETING
+  #define _ATL_XP_TARGETING // For using XP-compatible ATL/MFC functions
+ #endif // _ATL_XP_TARGETING
+#endif // _WIN32_WINNT <= 0x0600
+
+#ifndef __MINGW32__
+ #include <SDKDDKVer.h> // Doesn't exist in MinGW
+#endif // __MINGW32__
 
 // Windows Header Files
 #include <windows.h> // Main Windows header
