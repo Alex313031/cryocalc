b3fa5fba35dba4f7ae631e88755d2ba986264128
Alexander Frick <Alex313031@gmail.com>
2026-02-21 04:01:40 -0600

    update clang format settings and add clang format to osinfo submodule

--- clang_format.sh
+++ clang_format.sh
@@ -6,8 +6,8 @@ export HERE=${PWD}
 
 export CLANG_FORMAT_FILE=${HERE}/.clang-format
 
-clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/* &&
-
-clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/osinfo/* &&
+clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/{*.cc,*.h} &&
+clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/logging/{*.cc,*.h} &&
+clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/stress/{*.cc,*.h} &&
 
 exit 0
