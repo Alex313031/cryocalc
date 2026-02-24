9532d8c7b06518d7c9da176da3303c6b2989d10b
Alexander Frick <Alex313031@gmail.com>
2026-02-23 21:29:47 -0600

    update clang format configs and script again

--- clang_format.sh
+++ clang_format.sh
@@ -2,12 +2,8 @@
 
 # Copyright (c) 2026 Alex313031.
 
-export HERE=${PWD}
+export HERE=${PWD} &&
 
-export CLANG_FORMAT_FILE=${HERE}/.clang-format
+export CLANG_FORMAT_FILE=${HERE}/.clang-format &&
 
-clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/{*.cc,*.h} &&
-clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/logging/{*.cc,*.h} &&
-clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} ${HERE}/src/stress/{*.cc,*.h} &&
-
-exit 0
+clang-format --verbose -i --style=file:${CLANG_FORMAT_FILE} $@
