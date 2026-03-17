3c51b9d9bfb18e8df14baaa1b81f9a33902c69a5
Alexander Frick <Alex313031@gmail.com>
2026-03-17 17:01:27 -0500

    0.1.5

--- src/version.h
+++ src/version.h
@@ -76,7 +76,7 @@
 // Adhere to semver > semver.org
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 1
-#define BUILD_VERSION 4
+#define BUILD_VERSION 5
 
 #define VERSION_STRING _VERSION(MAJOR_VERSION, MINOR_VERSION, BUILD_VERSION)
 #define ABOUT_TITLE    L"About CryoCalc"
